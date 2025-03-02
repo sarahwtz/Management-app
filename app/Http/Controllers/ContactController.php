dd9d773125e2b9e5f61da1999cfbec8ad3a5f0ca
sarahwtz <sarahwtz@gmail.com>
2025-03-03 00:26:35 +0100

    Custom validation feedback messages for contact form.

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -51,13 +51,29 @@ class ContactController extends Controller
     }
     public function salvar(Request $request){
         //validacao:
-        $request->validate([
+       $regras = [
             'name'=>'required|min:3|max:40|unique:site_contatos',
             'telefone'=> 'required',
             'email' => 'email',
             'motivo_contatos_id' => 'required',
             'mensagem' => 'required |max:2000'
-        ]);
+       ];
+
+       $feedback = 
+        [
+          
+            'name.min' => 'O campo nome precisa ter no mínimo 3 caracteres',
+            'name.max' =>'O campo nome deve ter no maximo 40 caracteres',
+            'name.unique' => 'O nome informado ja está em uso',
+           
+          
+            'email.email'=> ' O email informado nao é válido',
+            'mensagem.max' =>'A mensagem deve ter no máximo 2000caracteres', 
+            'required' =>'O campo :attribute deve ser preenchido'
+        ];
+
+        $request->validate($regras, $feedback);
+        
         SiteContato::create($request->all());
         return redirect()->route('site.index');
         }
--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -1,4 +1,5 @@
  {{ $slot }}
+     {{-- 
  
  <form action={{ route('site.contato') }} method="post">
                     @csrf
@@ -40,6 +41,7 @@
                         <br>
                         <button type="submit" class="borda-preta">ENVIAR</button>
                     </form>
+                
                     @if($errors->any())
 
                     @foreach($errors->all() as $erro)
@@ -47,3 +49,55 @@
                     <br>
                     @endforeach
                     @endif
+                     --}}
+
+
+
+
+
+
+
+
+
+
+    <form action="{{ route('site.contato') }}" method="post">
+    @csrf
+    <input name="name" value="{{ old('name') }}" type="text" placeholder="Nome" class="{{ $classe }}">
+    @if($errors->has('name'))
+        <div class="error">{{ $errors->first('name') }}</div>
+    @endif
+
+    <br>
+    <input name="telefone" value="{{ old('telefone') }}" type="text" placeholder="Telefone" class="{{ $classe }}">
+    @if($errors->has('telefone'))
+        <div class="error">{{ $errors->first('telefone') }}</div>
+    @endif
+
+    <br>
+    <input name="email" value="{{ old('email') }}" type="text" placeholder="E-mail" class="{{ $classe }}">
+    @if($errors->has('email'))
+        <div class="error">{{ $errors->first('email') }}</div>
+    @endif
+    <br>
+
+    <select name="motivo_contatos_id" class="{{ $classe }}">
+        <option value="">Qual o motivo do contato?</option>
+        @foreach($motivo_contatos as $key => $motivo_contato)
+            <option value="{{ $motivo_contato->id }}" {{ old('motivo_contatos_id') == $motivo_contato->id ? 'selected' : '' }}>
+                {{ $motivo_contato->motivo_contato }}
+            </option>
+        @endforeach
+    </select>
+    @if($errors->has('motivo_contatos_id'))
+        <div class="error">{{ $errors->first('motivo_contatos_id') }}</div>
+    @endif
+
+    <br>
+    <textarea name="mensagem" class="{{ $classe }}">{{ old('mensagem') != '' ? old('mensagem') : 'Preencha aqui a sua mensagem' }}</textarea>
+    @if($errors->has('mensagem'))
+        <div class="error">{{ $errors->first('mensagem') }}</div>
+    @endif
+
+    <br>
+    <button type="submit" class="borda-preta">ENVIAR</button>
+</form>
