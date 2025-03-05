aa7294d2661e9440ebbcbdb3c6e4e5a7a1f2195a
sarahwtz <sarahwtz@gmail.com>
2025-03-05 23:18:30 +0100

    feat(middleware): Apply middleware to all routes.

--- app/Http/Controllers/SobreNosController.php
+++ app/Http/Controllers/SobreNosController.php
@@ -3,13 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Middleware\LogAcessoMiddleware;
 
 class SobreNosController extends Controller
 
-{   public function __construct() {
-    $this->middleware(LogAcessoMiddleware::class);
-}
+{  
 
     public function sobreNos(){
         /*echo 'sobre nós';*/
--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,9 @@ Route::get('/', function () {
 });
 */
 
-Route::middleware(LogAcessoMiddleware::class)->get('/','PrincipalController@principal')->name('site.index');
+Route::get('/','PrincipalController@principal')->name('site.index');
 Route::get('/sobre-nos','SobreNosController@sobreNos')->name('site.sobrenos');
-Route::middleware(LogAcessoMiddleware::class)->get('/contato', 'ContactController@contato')->name('site.contato');
+Route::get('/contato', 'ContactController@contato')->name('site.contato');
 Route::post('/contato', 'ContactController@salvar')->name('site.contato');
 Route::get('/login', function(){return 'Login'; })->name('site.login');
 
