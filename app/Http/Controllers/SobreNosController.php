00483b549b98819a9a1e2d41740a1bc94b4c15db
sarahwtz <sarahwtz@gmail.com>
2025-03-05 22:26:00 +0100

    feat(controller): Implement middleware in controller constructor SobreNosController.

--- app/Http/Controllers/SobreNosController.php
+++ app/Http/Controllers/SobreNosController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Middleware\LogAcessoMiddleware;
 
 class SobreNosController extends Controller
-{
+
+{   public function __construct() {
+    $this->middleware(LogAcessoMiddleware::class);
+}
+
     public function sobreNos(){
         /*echo 'sobre nós';*/
         return view('site.sobre-nos');
