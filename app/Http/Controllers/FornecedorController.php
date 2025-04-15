368442fc44444b887981d919a06f2ca4d9fc5b89
sarahwtz <sarahwtz@gmail.com>
2025-04-15 22:35:54 +0200

    Implement supplier registration: inclusion, search, and update functionalities.

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -3,43 +3,112 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Fornecedor;
 
 class FornecedorController extends Controller
 {
     public function index() {
-        /*$fornecedores = [
-            0=>[
-                'nome' => 'Fornecedor 1', 
-                'status' => 'N',
-                'cnpj' => '0',
-                'ddd' => '11',//sao paulo (SP)
-                'telefone' => '000-000',
-            ],
-            1 => [
-                'nome' => 'Fornecedor 2',
-                'status' => 'N',
-                'cnpj' => null,
-                'ddd' => '85', //fortaleza (CE)
-                'telefone' => '000-000',
-
-            ],
-            2 => [
-                'nome' => 'Fornecedor 3',
-                'status' => 'N',
-                'cnpj' => null,
-                'ddd' => '32',// juiz de fora (MG)
-                'telefone' => '000-000',
-
-            ],
-
-
-        ]; */
         
+       return view('app.fornecedor.index');
+    }
 
-    
+    public function listar(Request $request){
 
-       // return view('app.fornecedor.index', compact('fornecedores'));
-       return view('app.fornecedor');
+        $fornecedores = Fornecedor::where('nome', 'like', '%'.$request->input('nome').'%')
+        ->where('site', 'like', '%'.$request->input('site').'%')
+        ->where('uf', 'like', '%'.$request->input('uf').'%')
+        ->where('email', 'like', '%'.$request->input('email').'%')
+        ->get();
 
- }
+        return view('app.fornecedor.listar',['fornecedores' => $fornecedores]);
+
+       
+        
+    }
+
+
+    public function adicionar(Request $request){
+
+        $msg = '';
+
+        //inclusao
+        if ($request->input('_token') != '' && $request->input('id') == '') {
+
+            //validacao
+            $regras= [
+                'nome' => 'required| min:3|max:40',
+                'site' => 'required',
+                'uf' => 'required|min:2|max:2',
+                'email' => 'email'
+
+            ];
+
+            $feedback =[
+                'required' => ' O campo :attribute deve ser preenchido',
+                'nome.min' => 'O campo nome deve ter no minimo 3 caracteres',
+                'nome.max' => 'O campo nome deve ter no maximo 3 caracteres',
+                'uf.min' => 'O campo UF deve ter no minimo 2 caracteres',
+                'uf.max' => 'O campo UF deve ter no maximo 2 caracteres',
+                'email.email' => 'O campo e-mail nao foi preenchido corretamente',
+            ];
+
+            $request->validate($regras, $feedback);
+
+           $fornecedor = new Fornecedor();
+           $fornecedor->create($request->all());
+
+           //redirect
+
+           //dados view
+           $msg = 'Cadastro realizado com sucesso';
+        }
+
+        //edicao
+        if ($request->input('_token') != '' && $request->input('id') != '') {
+             // Regras de validação
+             $regras = [
+                'nome' => 'required|min:3|max:40',
+                'site' => 'required',
+                'uf' => 'required|min:2|max:2',
+                'email' => 'email'
+            ];
+        
+            $feedback = [
+                'required' => 'O campo :attribute deve ser preenchido',
+                'nome.min' => 'O campo nome deve ter no mínimo 3 caracteres',
+                'nome.max' => 'O campo nome deve ter no máximo 40 caracteres',
+                'uf.min' => 'O campo UF deve ter no mínimo 2 caracteres',
+                'uf.max' => 'O campo UF deve ter no máximo 2 caracteres',
+                'email.email' => 'O campo e-mail não foi preenchido corretamente',
+            ];
+        
+            $request->validate($regras, $feedback);
+          
+
+            $fornecedor = Fornecedor::find($request->input('id'));
+            $update = $fornecedor->update($request->all());
+
+            if($update) {
+                $msg = 'Update realizado com sucesso';
+            } else {
+                $msg =  'Erro ao tentar atualizar o registro';
+            }
+
+            return redirect()->route('app.fornecedor.editar', ['id' => $request->input('id'), 'msg' => $msg]);
+
+        }
+
+      
+        return view('app.fornecedor.adicionar',['msg' => $msg]);
+
+    }
+
+    public function editar($id, $msg = ''){
+        
+        $fornecedor = Fornecedor::find($id);
+
+        return view('app.fornecedor.adicionar', ['fornecedor' => $fornecedor, 'msg' => $msg]);
+    }
 }
+
+
