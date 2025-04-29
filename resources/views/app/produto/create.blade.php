8dc80e96dd873ec6dcd19548a2194a623b4de01b
sarahwtz <sarahwtz@gmail.com>
2025-04-29 14:41:40 +0200

    Refactor record editing: create new component for editing.

--- resources/views/app/produto/create.blade.php
+++ resources/views/app/produto/create.blade.php
@@ -7,7 +7,7 @@
 
 <div class="conteudo-pagina">
 
-    <div class="titulo-pagina-2">
+    <div class="titulo-pagina-2">    
         <p>Adicionar Produto</p>
     </div> 
 
@@ -20,35 +20,16 @@
         </div>  
 
             <div class="informacao-pagina">
-
-             <div style="width: 30%; margin-left: auto; margin-right: auto;">
-                <form method="post" action="{{ route('produto.store') }}">
-                @csrf
-                    <input type="text" name="nome" value="{{ old('nome') }}" placeholder="Nome" class="borda-preta">
-                    {{ $errors->has('nome') ? $errors->first('nome') : '' }}
-
-                    <input type="text" name="descricao" value="{{ old('descricao') }}" placeholder ="Descricao" class="borda-preta">
-                    {{ $errors->has('descricao') ? $errors->first('descricao') : '' }}
-
-                    <input type="text" name="peso" value="{{ old('peso') }}" placeholder= "Peso" class="borda-preta">
-                   {{ $errors->has('peso') ? $errors->first('peso') : '' }}
-
-
-                <select name="unidade_id">
-                <option>-- Selecione a Unidade de Medida --</option>
-               
-               @foreach($unidades as $unidade)
-                    <option value="{{ $unidade->id }}" {{ old('unidade_id') == $unidadeid ? 'selected' : '' }}>{{ $unidade->descricao }}</option>
-                    @endforeach
-                </select>
-                {{ $errors->has('unidade_id') ? $errors->first('unidade_id') : '' }}
-
-
-
-                    <button type="submit" class="borda-preta">Cadastrar</button>
-                 <form>   
+              <div style="width: 30%; margin-left: auto; margin-right: auto;">
+                  @component('app.produto._components.form_create_edit', ['unidades' => $unidades])
+                  @endcomponent
+                 
 
             </div>
+
+        </div>
+
+            
 </div>        
 
 @endsection
